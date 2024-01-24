88e3d774ddb40654cc105d3098113f63feb9b429
URROVA <arovaurrova@gmail.com>
2024-01-24 20:53:54 -0300

    Version final (segun yo). Ya podria ser publicable. Ammend: puse mal un pedazo de codigo; ya esta arreglado.

--- createPk3.bat
+++ createPk3.bat
@@ -2,4 +2,4 @@
 echo Creating PK3...
 
 cd pk3
-7z a -tzip ../file.pk3 *
+7z a -tzip ../build/IMPSCAPE.pk3 *
--- createPk7.bat
+++ createPk7.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Creating PK3...
+echo Creating PK7...
 
 cd pk3
-7z a -t7z ../file.pk7 *
+7z a -t7z ../build/IMPSCAPE.pk7 *
